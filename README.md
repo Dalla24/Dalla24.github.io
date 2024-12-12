9f7e260c9396488158eed573b3a2d34be445dee5
Luca Dalla Chiara <lucadalla88@gmail.com>
2024-12-12 18:05:26 +0100

    push

---
+++ README.md
@@ -0,0 +1,17 @@
+## Hi
+
+## Quick Start
+
+### 1. Edit [consts.ts](./consts.ts) file
+```ts
+export const domainUrl = 'yourdomain.com';
+export const projectName = 'yourprojectname';
+```
+
+### 2. Deploy using [SST](https://sst.dev)
+```bash
+npx sst deploy --stage production
+```
+
+## You don't have an AWS account?
+In order to self-host this project, you need an AWS account. If you don't have one, you can create one for free [here](https://aws.amazon.com/free/).
---
+++ sst.config.ts
@@ -0,0 +1,21 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+import {domainUrl, projectName} from "./consts";
+
+export default $config({
+  app(input) {
+    return {
+      name: `${projectName}`,
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.StaticSite("MWB", {
+      domain: {
+        name: `${domainUrl}`,
+        redirects: [`www.${domainUrl}`]
+      }
+    });
+  },
+});
